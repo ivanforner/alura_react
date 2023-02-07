dcaa3977d41a1756d358fc9578384d333eef78b7
Ivan Forner <ivanforner@gmail.com>
2023-02-07 14:18:44 -0300

    Aula 7

--- organo/src/components/Form/index.js
+++ organo/src/components/Form/index.js
@@ -1,7 +1,18 @@
 import "./Form.css";
 import TextField from "../TextField";
+import DropDown from "../DropDown";
 
 const Form = () => {
+    const itens = [
+        "Programação",
+        "Front-End",
+        "Data Science",
+        "DevOps",
+        "UX e Design",
+        "Mobile",
+        "Inovação e Gestão"
+    ];
+    
     return (
         <section className="form">
             <form>
@@ -9,6 +20,7 @@ const Form = () => {
                 <TextField label="Nome" placeholder="Digite o seu nome"/>
                 <TextField label="Cargo" placeholder="Digite seu cargo"/>
                 <TextField label="Imagem" placeholder="Digite o endereço da imagem"/>
+                <DropDown label="Time" itens={itens}/>
             </form>
         </section>
     )
