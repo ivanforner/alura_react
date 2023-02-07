e4e950d032c4c2b74a7e2d713df63826703656da
Ivan Forner <ivanforner@gmail.com>
2023-02-07 14:55:16 -0300

    Aula 9

--- organo/src/components/Form/index.js
+++ organo/src/components/Form/index.js
@@ -13,15 +13,20 @@ const Form = () => {
         "Mobile",
         "Inovação e Gestão"
     ];
+
+    const whenSubmmit = (event) => {
+        event.preventDefault();
+        console.log('Form foi submetido');
+    }
     
     return (
         <section className="form">
-            <form>
+            <form onSubmit={whenSubmmit}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <TextField label="Nome" placeholder="Digite o seu nome"/>
-                <TextField label="Cargo" placeholder="Digite seu cargo"/>
+                <TextField required={true} label="Nome" placeholder="Digite o seu nome"/>
+                <TextField required={true} label="Cargo" placeholder="Digite seu cargo"/>
                 <TextField label="Imagem" placeholder="Digite o endereço da imagem"/>
-                <DropDown label="Time" itens={itens}/>
+                <DropDown required={true} label="Time" itens={itens}/>
 
                 {/* Maneira diferente de passar propriedades */}
                 <Button>
